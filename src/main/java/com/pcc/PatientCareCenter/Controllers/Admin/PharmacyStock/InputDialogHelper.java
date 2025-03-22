316cf3b2ab3654efec871c6645713218cb0e10c5
Pravee <uzumakiprv@gmai.com>
2025-03-22 16:49:49 +0530

    Fixed combo boxes

--- src/main/java/com/pcc/PatientCareCenter/Controllers/Admin/PharmacyStock/InputDialogHelper.java
+++ src/main/java/com/pcc/PatientCareCenter/Controllers/Admin/PharmacyStock/InputDialogHelper.java
@@ -13,7 +13,6 @@ import java.sql.SQLException;
 import java.util.Arrays;
 import java.util.Locale;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 public class InputDialogHelper {
 
